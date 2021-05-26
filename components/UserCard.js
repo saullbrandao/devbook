a42c17a43f17eccad786f096f6e5d09c73138142
Saull Brandão <saull@outlook.com>
2021-05-26 09:34:13 -0300

    feat: change the the top users section to be a carousel

--- components/UserCard.js
+++ components/UserCard.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export const UserCard = () => {
   return (
-    <div className="w-52 2xl:w-72 rounded-md bg-background-paper p-2">
-      <Image className=' bg-gray-light rounded-md w-full' src='https://avatars.githubusercontent.com/u/1024025?v=4' width='300' height='300' />
+    <div className="w-56 2xl:ml-8 h-80 p-2 rounded-md bg-background-paper">
+      <Image className=' bg-gray-light rounded-md ' src='https://avatars.githubusercontent.com/u/1024025?v=4' width='300' height='300' />
       <div className="flex justify-between my-2">
         <h3 className='text-sm font-medium text-gray-200'>Linus Torvalds</h3>
         <h3 className='text-sm font-medium text-gray-200'>@torvalds</h3>
--- components/UserCardList.js
+++ components/UserCardList.js
@@ -1,18 +1,33 @@
+import Slider from "react-slick"
 import { UserCard } from "./UserCard"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 export const UserCardList = () => {
+  const settings = {
+    className: "center",
+    infinite: true,
+    slidesToShow: 5,
+    swipeToSlide: true,
+    // arrows: false,
+  };
+
   return (
     <div className='bg-background-paper-dark flex flex-col justify-between text-white rounded-md mb-8 mx-16 p-8'>
       <h1 className='text-4xl font-semibold mb-8'>Top users</h1>
 
-      <div className='flex justify-between'>
+      <Slider {...settings}>
         <UserCard />
         <UserCard />
         <UserCard />
         <UserCard />
         <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </Slider>
 
-      </div>
     </div >
   )
 }
