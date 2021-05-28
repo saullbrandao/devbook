6c2c9cfb7e0faf57632da52817fdb7d070122f85
Saull Brandão <saull@outlook.com>
2021-05-28 09:45:17 -0300

    feat: make possible to search for users

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,8 +1,25 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
 export const SearchBar = ({ rounded }) => {
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <div className={`bg-purple-light ${rounded ? 'rounded-full border-primary border-4' : 'rounded-md '} flex justify-between h-14`}>
-      <input className="rounded-full flex-1 bg-purple-light placeholder-gray-dark pl-4 text-md text-gray-dark focus:outline-none" type='text' placeholder='Search' />
-      <button className={`bg-primary flex-grow-0 ${rounded ? 'rounded-full' : 'rounded-r-md'}  focus:outline-none`}><img className=' px-3 w-11 ' src='search.svg' alt='search' /></button>
+      <input
+        className="rounded-full flex-1 bg-purple-light placeholder-gray-dark pl-4 text-md text-gray-dark focus:outline-none"
+        type='text'
+        placeholder='Search'
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button
+        className={`bg-primary flex-grow-0 ${rounded ? 'rounded-full' : 'rounded-r-md'}  focus:outline-none`}
+        onClick={() => searchTerm && router.push(`/search?query=${searchTerm || ''}`)}
+      >
+        <img className=' px-3 w-11 ' src='search.svg' alt='search' />
+      </button>
+
     </div>
   )
 }
