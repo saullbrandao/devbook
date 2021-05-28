d6c2a2d96edda3cfdb5977d65c508c9fed7b617a
Saull Brandão <saull@outlook.com>
2021-05-28 09:44:34 -0300

    feat: clicking on the logo sends you back to the home page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,15 @@
 import { SearchBar } from "./SearchBar"
-
+import Link from 'next/link'
 
 export const Navbar = () => {
   return (
     <div className='grid grid-cols-3 items-center py-4 mx-16'>
-      <img className='' src="logo.svg" alt="devbook" />
+      <Link href='/'>
+        <a>
+          <img className='' src="logo.svg" alt="devbook" />
+        </a>
+      </Link>
       <SearchBar rounded={true} />
-      {/* <div className='invisible' /> */}
     </div>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript, } from 'next/document'
-import { Navbar } from '../components/Navbar'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,7 +7,6 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet" />
-          <Navbar />
         </Head>
         <body>
           <Main />
