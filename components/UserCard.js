9aec412cf68e35719874517e541184856c97ea31
Saull Brandão <saull@outlook.com>
2021-05-29 09:43:02 -0300

    feat: redirect to the user page when clicking on the card

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import Image from 'next/image'
+import Link from 'next/link'
+
 import { useEffect, useState } from 'react'
 
 export const UserCard = ({ user_url }) => {
@@ -18,17 +20,19 @@ export const UserCard = ({ user_url }) => {
 
 
   return (
-    <div className="w-56 2xl:ml-8 h-80 p-2 rounded-md bg-background-paper ">
-      <div className="bg-gray-light rounded-md h-52 w-52">
-        {userInfo?.avatar_url &&
-          <Image src={userInfo.avatar_url} width='300' height='300' />
-        }
+    <Link href={`/users/${userInfo?.login}`}>
+      <div className="w-56 2xl:ml-8 h-80 p-2 rounded-md bg-background-paper cursor-pointer ">
+        <div className="bg-gray-light rounded-md h-52 w-52">
+          {userInfo?.avatar_url &&
+            <Image src={userInfo.avatar_url} width='300' height='300' />
+          }
+        </div>
+        <div className="flex justify-between my-2">
+          <h3 className='text-sm font-medium text-gray-200'>{userInfo?.name}</h3>
+          <h3 className='text-sm font-medium text-gray-200'>@{userInfo?.login}</h3>
+        </div>
+        <p className='text-xs my-2 line-clamp-4'>{userInfo?.bio ? userInfo.bio : 'No bio'}</p>
       </div>
-      <div className="flex justify-between my-2">
-        <h3 className='text-sm font-medium text-gray-200'>{userInfo?.name}</h3>
-        <h3 className='text-sm font-medium text-gray-200'>@{userInfo?.login}</h3>
-      </div>
-      <p className='text-xs my-2 line-clamp-4'>{userInfo?.bio ? userInfo.bio : 'No bio'}</p>
-    </div>
+    </Link>
   )
 }
