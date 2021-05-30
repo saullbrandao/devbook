f8cf5867f43fbb5e07b75d9176449560e03543dc
Saull Brandão <saull@outlook.com>
2021-05-30 09:18:14 -0300

    feat: allow to search for user by pressing the Enter key

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,9 +2,16 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
+
 export const SearchBar = ({ rounded }) => {
   const router = useRouter()
   const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearch = event => {
+    if (searchTerm && event.type === 'click' || event.key === 'Enter') {
+      router.push(`/search?query=${searchTerm}`)
+    }
+  }
 
   return (
     <div className={`bg-purple-light ${rounded ? 'rounded-full border-primary border-4' : 'rounded-md '} flex justify-between h-14`}>
@@ -13,10 +20,11 @@ export const SearchBar = ({ rounded }) => {
         type='text'
         placeholder='Search'
         onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyPress={handleSearch}
       />
       <button
         className={`bg-primary flex-grow-0 ${rounded ? 'rounded-full' : 'rounded-r-md'}  focus:outline-none`}
-        onClick={() => searchTerm && router.push(`/search?query=${searchTerm || ''}`)}
+        onClick={handleSearch}
       >
         <div className='px-2 py-2 w-11 flex justify-center'>
           <Image src='/search-icon.svg' alt='search' width='25' height='25' />
