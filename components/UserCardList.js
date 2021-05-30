9f5d130673ff59b5de046f7ee69b3e5e0fc7914e
Saull Brandão <saull@outlook.com>
2021-05-30 08:54:07 -0300

    refactor: use ssg on the home page

--- components/UserCardList.js
+++ components/UserCardList.js
@@ -1,20 +1,11 @@
 import Slider from "react-slick"
 import { UserCard } from "./UserCard"
+import { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useEffect, useState } from "react";
-import { searchApi } from "../services/api";
 
-const fetchTopUsers = async () => {
-  try {
-    const response = await searchApi.get('users?q=followers:%3E25000&per_page=10')
-    return response
-  } catch (error) {
-    console.log(error)
-  }
-}
 
-export const UserCardList = () => {
+export const UserCardList = ({ topUsers }) => {
   const settings = {
     className: "center",
     infinite: true,
@@ -22,36 +13,13 @@ export const UserCardList = () => {
     swipeToSlide: true,
   };
 
-  const [topUsers, setTopUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(async () => {
-    const response = await fetchTopUsers()
-    if (response.status === 200) {
-      const users = []
-      response.data.items.map(item => {
-        users.push({ url: item.url, id: item.id })
-      })
-      setTopUsers(users)
-      setLoading(false)
-    }
-  }, [])
-
   return (
     <div className='bg-background-paper-dark flex flex-col justify-between text-white rounded-md mb-8 p-8'>
       <h1 className='text-4xl font-semibold mb-8'>Top users</h1>
 
       <Slider {...settings}>
-        {topUsers?.map((user => {
-          return (loading ?
-            <UserCard key={user.id} />
-            :
-            <UserCard key={user.id} user_url={user.url} />
-          )
-        }))}
-
+        {topUsers?.map((user => <UserCard key={user.id} user_url={user.url} />))}
       </Slider>
-
     </div >
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import { SearchSection } from "../components/SearchSection";
 import { UserCardList } from "../components/UserCardList";
+import { searchApi } from '../services/api'
 
-export default function Home() {
+export default function Home({ topUsers }) {
   return (
     <div className='flex flex-col h-full gap-8 container mx-auto'>
       <SearchSection />
-      <UserCardList />
+      <UserCardList topUsers={topUsers} />
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const { data } = await searchApi.get('users?q=followers:%3E25000&per_page=10')
+
+  const topUsers = data.items.map(user => {
+    return {
+      id: user.id,
+      url: user.url
+    }
+  })
+
+  return {
+    props: {
+      topUsers
+    },
+    revalidate: 60 * 60 * 24
+  }
 }
