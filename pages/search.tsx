d7afad14bb4b57e5685368483ee81c795b542996
Saull Brandão <saull@outlook.com>
2021-06-04 09:11:43 -0300

    feat: add error message when failing to fetch data from github api and show skeletons when loading on the search results page

--- pages/search.tsx
+++ pages/search.tsx
@@ -7,11 +7,25 @@ export default function Search() {
   const router = useRouter()
   const term = router.query.query
 
-  const [loading, setLoading] = useState(true)
   const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const loadingCards = () => {
+    let cards = []
+    for (let i = 0; i < 8; i++) {
+      cards.push(<UserCard key={i} />)
+    }
+
+    return cards
+  }
+
 
   useEffect(() => {
     const searchUsers = async (term) => {
+      setIsError(false)
+      setIsLoading(true)
+
       try {
         const response = await searchApi.get(`users?q=${term}&per_page=8`)
         if (response.status === 200) {
@@ -20,11 +34,12 @@ export default function Search() {
             searchedUsers.push({ url: item.url, id: item.id })
           })
           setUsers(searchedUsers)
-          setLoading(false)
+          setIsLoading(false)
         }
       } catch (error) {
-        console.log(error)
+        setIsError(true)
       }
+
     }
     term && searchUsers(term)
   }, [term])
@@ -33,13 +48,12 @@ export default function Search() {
     <div className='container mx-auto flex flex-col items-center lg:items-stretch my-8 md:my-16 gap-8' >
       <h1 className='dark:text-white font-bold text-4xl'>Results for {term}</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-4 p-8 self-stretch rounded-md bg-secondary dark:bg-background-paper">
-        {users?.map((user => {
-          return (loading ?
-            <UserCard key={user.id} />
-            :
-            <UserCard key={user.id} user_url={user.url} />
-          )
-        }))}
+        {isError && <h1 className='dark:text-red-600 text-xl col-span-full' >Something went wrong...</h1>}
+        {isLoading
+          ? loadingCards()
+          : users?.map((user => <UserCard key={user.id} user_url={user.url} />))
+        }
+
       </div>
     </div>
   )
