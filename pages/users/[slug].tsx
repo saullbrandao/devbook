de4e72bda93a41afbb8fbb9be74962d44619dfcf
Saull Brandão <saull@outlook.com>
2021-06-04 09:40:56 -0300

    feat: add error message when failing to fetch data from github stats api and show messages when loading on the user page

--- pages/users/[slug].tsx
+++ pages/users/[slug].tsx
@@ -13,18 +13,29 @@ export default function User() {
 
   const [name, setName] = useState('')
   const [topRepos, setTopRepos] = useState([])
+  const [isLoadingData, setIsLoadingData] = useState(false)
+  const [isErrorData, setIsErrorData] = useState(false)
+  const [isLoadingRepos, setIsLoadingRepos] = useState(false)
+  const [isErrorRepos, setIsErrorRepos] = useState(false)
+
 
   useEffect(() => {
     const fetchUserData = async (slug) => {
+      setIsErrorData(false)
+      setIsLoadingData(true)
       try {
         const response = await usersApi.get(`/${slug}`)
         setName(response.data.name)
+        setIsLoadingData(false)
       } catch (error) {
-        console.log(error)
+        setIsErrorData(true)
       }
+
     }
 
     const fetchRepos = async (slug) => {
+      setIsErrorRepos(false)
+      setIsLoadingRepos(true)
       try {
         const response = await usersApi.get(`/${slug}/repos`, {
           params: {
@@ -35,8 +46,9 @@ export default function User() {
         })
         const sortedRepos = sortTopRepos(response.data)
         setTopRepos(sortedRepos)
+        setIsLoadingRepos(false)
       } catch (error) {
-        console.log(error)
+        setIsErrorRepos(true)
       }
     }
 
@@ -49,37 +61,47 @@ export default function User() {
   return (
     <section className='dark:text-white flex flex-col container items-center md:items-stretch  mx-auto gap-4 my-8'>
       <h1 className="font-bold text-4xl">{name}</h1>
-      <div>
+      <section>
         <h2 className='text-2xl mb-3'>▼ Github Stats</h2>
-        <div className='md:bg-secondary md:dark:bg-background-paper flex flex-col md:flex-row md:justify-around md:gap-4 md:p-4 w-full'>
-          <Image
-            src={`https://github-readme-stats.vercel.app/api?username=${slug}&show_icons=true&bg_color=151515&title_color=ffffff&text_color=9E9E9E&icon_color=67D47F`}
-            width='518'
-            height='210'
-          />
-          <Image
-            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${slug}&layout=compact&bg_color=151515&title_color=ffffff&text_color=9E9E9E`}
-            width='435'
-            height='210'
-          />
-        </div>
-      </div>
-      <div>
+        {isErrorData && <p className='text-red-600 text-lg my-2'>Something went wrong...</p>}
+        {isLoadingData
+          ? <p className='text-lg my-2'>Loading...</p>
+          :
+          <div className='md:bg-secondary md:dark:bg-background-paper flex flex-col md:flex-row md:justify-around md:gap-4 md:p-4 w-full'>
+            <Image
+              src={`https://github-readme-stats.vercel.app/api?username=${slug}&show_icons=true&bg_color=151515&title_color=ffffff&text_color=9E9E9E&icon_color=67D47F`}
+              width='518'
+              height='210'
+            />
+            <Image
+              src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${slug}&layout=compact&bg_color=151515&title_color=ffffff&text_color=9E9E9E`}
+              width='435'
+              height='210'
+            />
+          </div>
+        }
+      </section>
+      <section>
         <h2 className='text-2xl mb-3'>▼ Repos</h2>
-        <div className='md:bg-secondary md:dark:bg-background-paper flex flex-col md:flex-row md:justify-around gap-1 md:p-4'>
-          {topRepos?.map((repo, i) => {
-            return (
-              <div key={i} className='self-center'>
-                < Image
-                  src={`https://github-readme-stats.vercel.app/api/pin/?username=${slug}&repo=${repo.name}&bg_color=151515&title_color=ffffff&text_color=9E9E9E`}
-                  width='435'
-                  height='130'
-                />
-              </div>
-            )
-          })}
-        </div>
-      </div>
+        {isErrorRepos && <p className='text-red-600 text-lg my-2'>Something went wrong...</p>}
+        {isLoadingRepos
+          ? <p className='text-lg my-2'>Loading...</p>
+          :
+          <div className='md:bg-secondary md:dark:bg-background-paper flex flex-col md:flex-row md:justify-around gap-1 md:p-4'>
+            {topRepos?.map((repo, i) => {
+              return (
+                <div key={i} className='self-center'>
+                  < Image
+                    src={`https://github-readme-stats.vercel.app/api/pin/?username=${slug}&repo=${repo.name}&bg_color=151515&title_color=ffffff&text_color=9E9E9E`}
+                    width='435'
+                    height='130'
+                  />
+                </div>
+              )
+            })}
+          </div>
+        }
+      </section>
     </section>
   )
 }
