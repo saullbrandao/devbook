bcd35a1a3cde1e83946b25c3592402237e2ade82
Saull Brandão <saull@outlook.com>
2021-09-10 13:34:07 -0300

    feat: change header text

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -6,7 +6,7 @@ export const SearchSection = () => {
     <div className='bg-secondary dark:bg-background-paper flex justify-between rounded-md p-2 mt-4 md:my-8 md:px-16' >
       <div className='flex flex-col justify-evenly max-w-md'>
         <h1 className='dark:text-white text-4xl sm:text-5xl md:text-6xl'>
-          Stalk people on github!
+          Find people on github!
         </h1>
         <SearchBar hidden />
       </div>
