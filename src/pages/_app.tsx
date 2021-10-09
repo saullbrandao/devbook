06e141f7195817ad1a81f5b579824e314c76f531
Saull Brandão <saull@outlook.com>
2021-10-09 09:31:51 -0300

    fix: correct import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { AppProps } from 'next/dist/shared/lib/router/router'
 import { Navbar } from 'components/Navbar'
 import Script from 'next/script'
 import Head from 'next/head'
@@ -8,7 +8,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-       <meta name='description'  content='Shows statistics about github users'/>
+        <meta
+          name="description"
+          content="Shows statistics about github users"
+        />
       </Head>
       <Script strategy="beforeInteractive" src="/scripts/darkMode.js" />
       <Navbar />
